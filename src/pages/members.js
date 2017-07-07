845ce1cc79a46a16c008cdca59235805cc04aab0
Joever Monceda <joever.monceda@gmail.com>
2017-07-07 13:38:03 +0800

    separate INVITE USER, OPEN CHANNEL , MEMBER styles from component

--- src/pages/members.js
+++ src/pages/members.js
@@ -12,6 +12,7 @@ import {APP_ID, PULLDOWN_DISTANCE} from '../consts';
 import TopBar from '../components/topBar';
 import moment from 'moment';
 import SendBird from 'sendbird';
+import styles from './styles/member';
 var sb = null;
 
 export default class Members extends Component {
@@ -87,51 +88,3 @@ export default class Members extends Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    backgroundColor: '#ffffff'
-  },
-  listContainer: {
-    flex: 11,
-    justifyContent: 'center',
-    alignItems: 'stretch'
-  },
-  listItem: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f7f8fc',
-    borderBottomWidth: 0.5,
-    borderColor: '#D0DBE4',
-    padding: 5
-  },
-  listIcon: {
-    justifyContent: 'flex-start',
-    paddingLeft: 10,
-    paddingRight: 15
-  },
-  profileIcon: {
-    width: 30,
-    height: 30
-  },
-  listInfo: {
-    flex: 1,
-    justifyContent: 'flex-start'
-  },
-  titleLabel: {
-    fontSize: 15,
-    fontWeight: '600',
-    color: '#60768b',
-  },
-  descText: {
-    textAlign: 'center',
-    fontSize: 12,
-    color: '#ababab',
-    fontWeight: 'bold'
-  }
-});
---
+++ src/pages/styles/openChannel.js
@@ -0,0 +1,48 @@
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: '#ffffff'
+  },
+  listContainer: {
+    flex: 11,
+    justifyContent: 'center',
+    alignItems: 'stretch'
+  },
+  listItem: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f7f8fc',
+    borderBottomWidth: 0.5,
+    borderColor: '#D0DBE4',
+    padding: 5
+  },
+  listIcon: {
+    justifyContent: 'flex-start',
+    paddingLeft: 10,
+    paddingRight: 15
+  },
+  channelIcon: {
+    width: 30,
+    height: 30
+  },
+  listInfo: {
+    flex: 1,
+    justifyContent: 'flex-start'
+  },
+  titleLabel: {
+    fontSize: 15,
+    fontWeight: '600',
+    color: '#60768b',
+  },
+  memberLabel: {
+    fontSize: 13,
+    fontWeight: '400',
+    color: '#abb8c4',
+  }
+});
